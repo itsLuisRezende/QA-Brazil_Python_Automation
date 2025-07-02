58e19dc63699c8c217ffae1e2a4b9a92455b6328
Luis Fernando <140192412+itsLuisRezende@users.noreply.github.com>
2025-07-02 16:48:13 -0300

    commit (tarefa 3 concluída)

--- main.py
+++ main.py
@@ -1,2 +1,39 @@
-
+import data
 class TestUrbanRoutes:
+    def test_set_route(self): #Adicionar em S8
+        print("Função criada para definir rota.")
+        pass
+
+    def test_select_plan(self): #Adicionar em S8
+        print("Função criada para selecionar plano.")
+        pass
+
+    def test_fill_phone_number(self): #Adicionar em S8
+        print("Função criada para preencher número de telefone.")
+        pass
+
+    def test_fill_card(self): #Adicionar em S8
+        print("Função criada para preencher cartão.")
+        pass
+
+    def test_comment_for_driver(self): #Adicionar em S8
+        print("Função criada para comentar com o motorista.")
+        pass
+
+    def test_order_blanket_and_handkerchiefs(self): #Adicionar em S8
+        print("Função criada para encomendar cobertores e lençóis.")
+        pass
+
+    def test_order_2_ice_creams(self): #Adicionar em S8
+        print("Função criada para pedido de 2 sorvetes.")
+        pass
+
+    def test_car_search_model_appears(self): #Adicionar em S8
+        print("Função criada para aparecer modelo de busca de carros.")
+        pass
+
+
+
+
+
+
